72eaaef4915a1aa2f3f6f4da781a78f1f25f3e72
Salim Cherifi <salim.cherifi@master-developpement-logiciel.fr>
2020-02-27 15:31:26 +0100

    change to vue and begin

---
+++ Front/borderline-and-co-front/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" class="mt-0">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <router-link to="/IHMUtilisateur">
+          <a class="nav-link" href="#">IHM utilisateur</a>
+        </router-link>
+        <router-link to="/IHMExpert">
+        <a class="nav-link" href="#">IHM expert</a>
+        </router-link>
+    </nav>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
