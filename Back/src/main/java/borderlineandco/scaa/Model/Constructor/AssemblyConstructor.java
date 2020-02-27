79b43dcf91d5902179d850dd2735e67bb814394b
tudal <patrick.tudal@master-developpement-logiciel.fr>
2020-02-27 13:50:22 +0100

    Add implementation link and rename

---
+++ Back/src/main/java/borderlineandco/scaa/Model/Constructor/AssemblyConstructor.java
@@ -0,0 +1,17 @@
+package borderlineandco.scaa.Model.Constructor;
+
+import borderlineandco.scaa.Interface.ICreateMAS;
+import borderlineandco.scaa.Model.Domain.Entities.ComponentEntity;
+
+public class AssemblyConstructor implements ICreateMAS {
+
+    @Override
+    public void createAgentByComponent(ComponentEntity c) {
+
+    }
+
+    @Override
+    public void launchMAS() {
+
+    }
+}
