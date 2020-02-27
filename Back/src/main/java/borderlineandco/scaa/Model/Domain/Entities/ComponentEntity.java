20d4f7958ab02b278c79e7d36524c799babe93b2
RaphaelGouesnard <raphix.g@hotmail.fr>
2020-02-27 11:56:01 +0100

    ajout des map entity

--- Back/src/main/java/borderlineandco/scaa/Model/Domain/Entities/ComponentEntity.java
+++ Back/src/main/java/borderlineandco/scaa/Model/Domain/Entities/ComponentEntity.java
@@ -1,9 +1,6 @@
 package borderlineandco.scaa.Model.Domain.Entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.List;
@@ -17,10 +14,17 @@ public class ComponentEntity {
 
     @NotNull
     private String name;
-
+    
     List<PortEntity> listPortIn;
 
     List<PortEntity> listPortOut;
+
+    @ManyToMany
+    @JoinTable(
+            name = "assembly_composant",
+            joinColumns = @JoinColumn(name = "composant_id"),
+            inverseJoinColumns = @JoinColumn(name = "assembly_id"))
+    List<AssemblyEntity> assemblys;
 
     public Long getId() {
         return id;
