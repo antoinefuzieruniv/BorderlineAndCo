2a31220260a78a16d4ca8f3d32c92ebead6452c1
tudal <patrick.tudal@master-developpement-logiciel.fr>
2020-02-27 14:06:05 +0100

    correct implementation link

--- Back/src/main/java/borderlineandco/scaa/Model/Domain/Services/ComponentService.java
+++ Back/src/main/java/borderlineandco/scaa/Model/Domain/Services/ComponentService.java
@@ -1,7 +1,28 @@
 package borderlineandco.scaa.Model.Domain.Services;
 
+import borderlineandco.scaa.Interface.IGetPreferences;
+import borderlineandco.scaa.Interface.IProvide;
+import borderlineandco.scaa.Interface.ISaveChoice;
+import borderlineandco.scaa.Model.Domain.Entities.ComponentEntity;
+import borderlineandco.scaa.Model.Domain.Entities.ConnectionEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ComponentService {
+public class ComponentService implements IProvide, IGetPreferences, ISaveChoice {
+    @Override
+    public List<ComponentEntity> provideComponents() {
+        return null;
+    }
+
+    @Override
+    public List<ConnectionEntity> getPreferedConnections() {
+        return null;
+    }
+
+    @Override
+    public void saveChoice(ConnectionEntity connectionEntity) {
+
+    }
 }
