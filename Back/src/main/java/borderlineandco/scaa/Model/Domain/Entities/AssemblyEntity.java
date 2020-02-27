492781603c0e6fa0f654bf1e1fdcbb30cc808ec2
RaphaelGouesnard <raphix.g@hotmail.fr>
2020-02-27 11:30:40 +0100

    implementation des Entity

--- Back/src/main/java/borderlineandco/scaa/Model/Domain/Entities/AssemblyEntity.java
+++ Back/src/main/java/borderlineandco/scaa/Model/Domain/Entities/AssemblyEntity.java
@@ -1,7 +1,21 @@
 package borderlineandco.scaa.Model.Domain.Entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
 
 @Entity
 public class AssemblyEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    private List<ComponentEntity> listComponent;
+
+    private List<ConnectionEntity> listConnection;
+
+
 }
