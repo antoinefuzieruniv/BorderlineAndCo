0c80eb9e3fae4ef9140a04eb009a87fb9bde7043
tudal <patrick.tudal@master-developpement-logiciel.fr>
2020-02-27 10:54:17 +0100

    add methods

--- Back/src/main/java/borderlineandco/scaa/Interface/IChooseConnection.java
+++ Back/src/main/java/borderlineandco/scaa/Interface/IChooseConnection.java
@@ -1,4 +1,12 @@
 package borderlineandco.scaa.Interface;
 
+import borderlineandco.scaa.Model.Domain.Entities.ConnectionEntity;
+
+import java.util.List;
+
 public interface IChooseConnection {
+
+    List<ConnectionEntity> provideConnections();
+
+    ConnectionEntity selectConnection();
 }
