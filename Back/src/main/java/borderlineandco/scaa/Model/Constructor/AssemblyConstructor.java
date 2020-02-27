285856517583ab787091d2d846c9bc8935af00f3
tudal <patrick.tudal@master-developpement-logiciel.fr>
2020-02-27 13:54:14 +0100

    Add implementation link

--- Back/src/main/java/borderlineandco/scaa/Model/Constructor/AssemblyConstructor.java
+++ Back/src/main/java/borderlineandco/scaa/Model/Constructor/AssemblyConstructor.java
@@ -1,9 +1,14 @@
 package borderlineandco.scaa.Model.Constructor;
 
 import borderlineandco.scaa.Interface.ICreateMAS;
+import borderlineandco.scaa.Interface.IGetPreferences;
+import borderlineandco.scaa.Interface.ISaveChoice;
 import borderlineandco.scaa.Model.Domain.Entities.ComponentEntity;
+import borderlineandco.scaa.Model.Domain.Entities.ConnectionEntity;
 
-public class AssemblyConstructor implements ICreateMAS {
+import java.util.List;
+
+public class AssemblyConstructor implements ICreateMAS, IGetPreferences, ISaveChoice {
 
     @Override
     public void createAgentByComponent(ComponentEntity c) {
@@ -12,6 +17,16 @@ public class AssemblyConstructor implements ICreateMAS {
 
     @Override
     public void launchMAS() {
+
+    }
+
+    @Override
+    public List<ConnectionEntity> getPreferedConnections() {
+        return null;
+    }
+
+    @Override
+    public void saveChoice(ConnectionEntity connectionEntity) {
 
     }
 }
