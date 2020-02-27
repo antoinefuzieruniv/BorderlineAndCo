c52a83295384cef119e84b37209687df63a11ea0
Antoine Fuzier <antoine.fuzier@master-developpement-logiciel.fr>
2020-02-27 11:36:26 +0100

    test 1 2 1 2

--- Back/src/main/java/borderlineandco/scaa/Model/Domain/Entities/ComponentEntity.java
+++ Back/src/main/java/borderlineandco/scaa/Model/Domain/Entities/ComponentEntity.java
@@ -1,7 +1,17 @@
 package borderlineandco.scaa.Model.Domain.Entities;
 
+import MASInfrastructure.Agent.InfraAgent;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class ComponentEntity {
+
+    @Id
+    private Long id;
+
+
+
+
 }
