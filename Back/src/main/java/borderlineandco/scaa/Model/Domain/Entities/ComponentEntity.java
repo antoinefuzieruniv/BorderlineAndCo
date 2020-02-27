8e0c1f938a51dd850ed7fbb60a5329cb7dd8de60
RaphaelGouesnard <raphix.g@hotmail.fr>
2020-02-27 11:33:22 +0100

    ajout set/get entity

--- Back/src/main/java/borderlineandco/scaa/Model/Domain/Entities/ComponentEntity.java
+++ Back/src/main/java/borderlineandco/scaa/Model/Domain/Entities/ComponentEntity.java
@@ -22,4 +22,35 @@ public class ComponentEntity {
 
     List<PortEntity> listPortOut;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<PortEntity> getListPortIn() {
+        return listPortIn;
+    }
+
+    public void setListPortIn(List<PortEntity> listPortIn) {
+        this.listPortIn = listPortIn;
+    }
+
+    public List<PortEntity> getListPortOut() {
+        return listPortOut;
+    }
+
+    public void setListPortOut(List<PortEntity> listPortOut) {
+        this.listPortOut = listPortOut;
+    }
 }
--- Back/src/main/java/borderlineandco/scaa/Model/Domain/Entities/PortEntity.java
+++ Back/src/main/java/borderlineandco/scaa/Model/Domain/Entities/PortEntity.java
@@ -21,4 +21,28 @@ public class PortEntity {
     @NotNull
     @NotEmpty
     private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
