33a96e216e4c122c0b591bd6052fff78978ea8c1
RaphaelGouesnard <raphix.g@hotmail.fr>
2020-02-27 11:30:40 +0100

    implementation des Entity

--- Back/src/main/java/borderlineandco/scaa/Model/Domain/Entities/ComponentEntity.java
+++ Back/src/main/java/borderlineandco/scaa/Model/Domain/Entities/ComponentEntity.java
@@ -3,15 +3,25 @@ package borderlineandco.scaa.Model.Domain.Entities;
 import MASInfrastructure.Agent.InfraAgent;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Entity
 public class ComponentEntity {
 
     @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
 
+    @NotNull
+    private String name;
 
+    List<PortEntity> listPortIn;
 
+    List<PortEntity> listPortOut;
 
 }
--- Back/src/main/java/borderlineandco/scaa/Model/Domain/Entities/PortEntity.java
+++ Back/src/main/java/borderlineandco/scaa/Model/Domain/Entities/PortEntity.java
@@ -1,7 +1,24 @@
 package borderlineandco.scaa.Model.Domain.Entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class PortEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @NotEmpty
+    private String name;
+
+    @NotNull
+    @NotEmpty
+    private String type;
 }
